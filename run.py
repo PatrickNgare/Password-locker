6fe5eacf4cc8a31fa0c51da7f6749bbe389af676
patrickNgare <patrickngare@gmail.com>
2018-01-21 12:31:59 +0300

    created create find save check_user and create credentials function

--- run.py
+++ run.py
@@ -5,3 +5,19 @@ from password import User, Credentials
 def create_user(user_name, password):
     new_user = User(user_name, password)
     return new_use
+def save_users(user):
+    user.save_user()
+
+
+def find_user(name):
+    return User.find_by_name(name)
+
+
+def check_user_exists(user_name, password):
+    if_user_exist = Credentials.user_exist(user_name,password)
+    return if_user_exist
+
+def create_credential(account_name,account_password):
+    new_credential = Credentials(account_name,account_password)
+
+    return new_credential    
